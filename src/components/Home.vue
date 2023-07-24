97df3f27686e94f6b719a9d1258edddd93ca57f6
nicolasloison13 <nicolasloison13@icloud.com>
2023-07-24 09:07:19 +0200

    create new components HomeProjects

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,13 @@
 <script>
   import Hero from "@/components/home/Hero.vue";
   import About from "@/components/home/About.vue";
+  import HomeProjects from "@/components/home/HomeProjects.vue";
 
   export default {
     components: {
       Hero,
-      About
+      About,
+      HomeProjects
     },
   };
 </script>
@@ -14,6 +16,7 @@
   <div>
     <Hero />
     <About />
+    <HomeProjects />
   </div>
 </template>
 
