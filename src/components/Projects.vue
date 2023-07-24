b28e7d5a1e157befdb2165f3f03ed7b6e636ac31
nicolasloison13 <nicolasloison13@icloud.com>
2023-07-24 16:31:58 +0200

    create new components projects1 and init projects page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,29 @@
-<script setup>
+<script>
 
+import projects1 from "@/components/projects/projects1.vue";
+
+export default {
+  components: {
+    projects1
+  },
+};
 </script>
 
 <template>
-  <h2>Projects</h2>
+  <section class="projects" id="projects">
+    <h2><span>MY</span> PROJECTS</h2>
+    <projects1 />
+  </section>
 </template>
 
 <style scoped>
+h2 {
+  font-size: var(--h1-font);
+  font-weight: 500;
+}
+
+h2 span {
+  color: var(--primary-color);
+}
 
 </style>
