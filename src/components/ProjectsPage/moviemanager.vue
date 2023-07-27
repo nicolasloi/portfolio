5baa8863244e6725cfe9ad5870c5e7ed24999d0c
nicolasloison13 <nicolasloison13@icloud.com>
2023-07-27 16:49:04 +0200

    add tools + img in page movielist

--- src/components/ProjectsPage/moviemanager.vue
+++ src/components/ProjectsPage/moviemanager.vue
@@ -11,6 +11,19 @@
     <p>This project is a movie manager that allows users to store the movies they have watched and rate them on a
       scale of 1 to 5 stars, as well as leave a comment.
       Users can log in, log out, and register to access their movie list.</p>
+    <img src="../../assets/home_movieList.png" alt="screen movieManager">
+    <img src="../../assets/screen_movieManager.png" alt="screen movieManager">
+    <img src="../../assets/create_movieList.png" alt="screen movieManager">
+
+    <h3>Tools</h3>
+    <div class="tools">
+      <h6>Laravel</h6>
+      <h6>Tailwind</h6>
+      <h6>DaisyUI</h6>
+      <h6>Meilisearch</h6>
+      <h6>TMDB API</h6>
+    </div>
+
   </section>
 </template>
 
@@ -47,4 +60,34 @@ h2 {
   font-weight: 400;
   color: var(--primary-color);
 }
+
+img {
+  max-width: 100%;
+  width: auto;
+  margin-top: 2rem;
+  margin-right: 1.50rem;
+}
+
+h3 {
+  font-size: 2rem;
+  font-weight: 400;
+  margin-top: 2rem;
+}
+
+.tools {
+  margin-top: 1rem;
+  display: flex;
+  gap: 0.25rem;
+  flex-wrap: wrap;
+}
+
+.tools h6 {
+  border-radius: 0.625rem;
+  border: 1px solid #1097FF;
+
+  font-size: 0.725rem;
+  font-weight: 400;
+  padding: 5px;
+}
+
 </style>
