768d81e6bf1e97e5379f4692d029c76f5ac718eb
nicolasloison13 <nicolasloison13@icloud.com>
2023-07-27 10:49:51 +0200

    edit notFound page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-  <h2>404</h2>
+  <section>
+    <h2>Page not found (404)</h2>
+  </section>
 </template>
 
 <style scoped>
-
+  h2 {
+    margin-top: 5rem;
+    font-size: var(--h1-font);
+  }
 </style>
