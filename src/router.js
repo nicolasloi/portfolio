9ffc540747cec3a52da9db36eb3f3d9986998d6e
nicolasloison13 <nicolasloison13@icloud.com>
2023-07-27 11:16:42 +0200

    add routes for projects and create pages for all projects

--- src/router.js
+++ src/router.js
@@ -2,11 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import Projects from '@/components/Projects.vue'
 import NotFound from '@/components/NotFound.vue'
+import moviemanager from "@/components/ProjectsPage/moviemanager.vue";
+import umbrella from "@/components/ProjectsPage/umbrella.vue";
+import jobtreksuivis from "@/components/ProjectsPage/jobtreksuivis.vue";
+import savoria from "@/components/ProjectsPage/savoria.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/projects', component: Projects },
-    { path: '/:pathMatch(.*)*', component: NotFound }
+    { path: '/:pathMatch(.*)*', component: NotFound },
+    { path: '/projects/moviemanager', component: moviemanager },
+    { path: '/projects/umbrella', component: umbrella },
+    { path: '/projects/jobtreksuivis', component: jobtreksuivis },
+    { path: '/projects/savoria', component: savoria },
 ]
 
 const router = createRouter({
